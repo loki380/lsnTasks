7ff1c8704d0a491ed82a2b5b2da64684bc2449a9
kamolsz2 <kamil.olszewski@pg.edu.pl>
2023-02-14 12:52:19 +0100

    second task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Task.resolveFirstTask();
+//		Task.resolveFirstTask();
+		Task.resolveSecondTask();
 	}
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class Task {
 
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void resolveFirstTask() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter input: ");
 		String input = br.readLine();
 
@@ -36,11 +41,38 @@ public class Task {
 		System.out.println("max: " + max);
 	}
 
-	private static void resolveSecondTask() {
+	public static void resolveSecondTask() throws IOException {
+		System.out.println("Enter input: ");
+		String input = br.readLine();
+
+		String[] splited = input.split(" ");
+
+		List<Integer> inputAsListIntegers = Arrays.stream(splited)
+				.map(Integer::valueOf)
+				.sorted()
+				.collect(Collectors.toList());
+
+		List<Integer> usedIntegers = new ArrayList<>();
+
+		List<Pair<Integer, Integer>> map = inputAsListIntegers.stream()
+				.map(integer -> {
+					int searchInt = 13 - integer;
+					if (inputAsListIntegers.contains(searchInt)) {
+						usedIntegers.add(searchInt);
+						return new ImmutablePair<>(integer, searchInt);
+					} else {
+						return null;
+					}
+				})
+				.filter(Objects::nonNull)
+				.filter(i -> !usedIntegers.contains(i.getKey()))
+				.collect(Collectors.toList());
+
+		map.forEach(pair -> System.out.println(pair.getLeft() + " " + pair.getRight()));
 
 	}
 
-	private static void resolveThirdTask() {
+	public static void resolveThirdTask() {
 
 	}
 
