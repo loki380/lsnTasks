8d14d04c433b732969e4733615646b08b0b4cfa0
kamolsz2 <kamil.olszewski@pg.edu.pl>
2023-02-17 21:02:59 +0100

    task menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
-//		Task.resolveFirstTask();
-//		Task.resolveSecondTask();
-		Task.resolveThirdTask();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Choose task: ");
+		Integer taskNumber = Integer.valueOf(br.readLine());
+
+		if (taskNumber == 1) {
+			Task.resolveFirstTask();
+		} else if (taskNumber == 2) {
+			Task.resolveSecondTask();
+		} else if (taskNumber == 3) {
+			Task.resolveThirdTask();
+		}
 	}
 }
