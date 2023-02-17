fe5888ca18ffcd97f8b570e367757c231454f5dc
kamolsz2 <kamil.olszewski@pg.edu.pl>
2023-02-17 20:59:57 +0100

    third task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -72,7 +72,56 @@ public class Task {
 
 	}
 
-	public static void resolveThirdTask() {
+	public static void resolveThirdTask() throws IOException {
+		System.out.println("Enter input: ");
+		int connections = Integer.parseInt(br.readLine());
+
+		List<Pair<Integer, Integer>> pairs = new ArrayList<>();
+
+		for (int i = 0; i < connections; i++) {
+			String connection = br.readLine();
+
+			String[] splited = connection.split(" ");
+
+			List<Integer> inputAsListIntegers = Arrays.stream(splited)
+					.map(Integer::valueOf)
+					.collect(Collectors.toList());
+
+			pairs.add(new ImmutablePair<>(inputAsListIntegers.get(0), inputAsListIntegers.get(1)));
+		}
+
+		List<List<Integer>> groupsOfVertices = new ArrayList<>();
+		List<Pair<Integer, Integer>> tmpPairs = new ArrayList<>(pairs);
+
+		pairs.forEach(pair -> {
+			if (tmpPairs.contains(pair)) {
+				List<Integer> groupOfVertices = new ArrayList<>(Arrays.asList(pair.getLeft(), pair.getRight()));
+				tmpPairs.remove(pair);
+
+				Pair<Integer, Integer> leftPair = searchPairForVerticle(pair.getLeft(), tmpPairs);
+				if (leftPair != null) {
+					tmpPairs.remove(leftPair);
+					groupOfVertices.add(leftPair.getLeft());
+					groupOfVertices.add(leftPair.getRight());
+				}
+
+				Pair<Integer, Integer> rightPair = searchPairForVerticle(pair.getRight(), tmpPairs);
+				if (rightPair != null) {
+					tmpPairs.remove(rightPair);
+					groupOfVertices.add(rightPair.getLeft());
+					groupOfVertices.add(rightPair.getRight());
+				}
+
+				groupsOfVertices.add(groupOfVertices.stream().distinct().collect(Collectors.toList()));
+			}
+		});
+
+		System.out.println("Output: ");
+		groupsOfVertices.forEach(group -> System.out.println(group.stream().map(v -> "[" + v.toString() + "]").collect(Collectors.joining("-"))));
+	}
+
+	private static Pair<Integer, Integer> searchPairForVerticle(int verticleValue, List<Pair<Integer, Integer>> pairs) {
+		return pairs.stream().filter(p -> p.getLeft() == verticleValue || p.getRight() == verticleValue).findAny().orElse(null);
 
 	}
 
